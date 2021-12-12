77686efb89a22260c54297daf4817ab77b2f80eb
stephany0318 <stephany.bermudez.mt@correo.usa.edu.co>
2021-12-12 00:12:14 -0500

    first commit

---
+++ src/main/java/Chocolateproject/Chocolatewilli/ChocolatewilliApplication.java
@@ -0,0 +1,13 @@
+package Chocolateproject.Chocolatewilli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolatewilliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocolatewilliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Chocolateproject/Chocolatewilli/ChocolatewilliApplicationTests.java
@@ -0,0 +1,13 @@
+package Chocolateproject.Chocolatewilli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolatewilliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
